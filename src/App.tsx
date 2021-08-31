c7bd677073db1743fcb4b817358127c71c369a7c
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-30 22:41:53 -0500

    :dart: Improve: agregar configuracion de webpack para desarrollo y produccion

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ImageTwo from "./react-1.svg";
 export default function App() {
     return (
         <div>
-            <h1>React y webpack5 template</h1>
+            <h1>React y webpack5 template - {process.env.NODE_ENV} - {process.env.name}</h1>
             <img src={Image} alt="React image" />
             <img src={ImageTwo} alt="React image" width="300" />
         </div>
