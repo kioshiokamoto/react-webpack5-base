05c3b1a1c7314addf0d09fe650474b9e5490adbd
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-30 23:41:57 -0500

    Prueba 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import './styles.css'
 import Image from './react.png'
 import ImageTwo from './react-1.svg'
 export default function App() {
-  const nombre = "Okamoto Rojas"
+  const nombre = 'Okamoto'
   return (
     <div>
       <h1>
-        React y webpack5 template - {process.env.NODE_ENV} - {process.env.name} {nombre}
+        React y webpack5 template - {process.env.NODE_ENV} - {process.env.name}{' '}
+        {nombre}
       </h1>
       <img src={Image} alt="React 1" />
       <img src={ImageTwo} alt="React 2" width="300" />
