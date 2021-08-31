f07b16adae8df65001743d784c7a3f8903f50a29
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-30 23:38:12 -0500

    :dart: Improve: agregar husky y lint-state

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import './styles.css'
 import Image from './react.png'
 import ImageTwo from './react-1.svg'
 export default function App() {
+  const nombre = "Okamoto"
   return (
     <div>
       <h1>
-        React y webpack5 template - {process.env.NODE_ENV} - {process.env.name}
+        React y webpack5 template - {process.env.NODE_ENV} - {process.env.name} {nombre}
       </h1>
       <img src={Image} alt="React 1" />
       <img src={ImageTwo} alt="React 2" width="300" />
