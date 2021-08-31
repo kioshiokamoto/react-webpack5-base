7daf243f29653747e77f518dfae8ee008408c300
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-30 23:05:13 -0500

    :dart: Improve: agregar linter

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import ImageTwo from "./react-1.svg";
 export default function App() {
     return (
         <div>
-            <h1>React y webpack5 template - {process.env.NODE_ENV} - {process.env.name}</h1>
-            <img src={Image} alt="React image" />
-            <img src={ImageTwo} alt="React image" width="300" />
+            <h1>
+                React y webpack5 template - {process.env.NODE_ENV} -{" "}
+                {process.env.name}
+            </h1>
+            <img src={Image} alt="React 1" />
+            <img src={ImageTwo} alt="React 2" width="300" />
         </div>
     );
 }
