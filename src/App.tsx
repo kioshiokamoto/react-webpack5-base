d09c0e81000695595efe27d894f20f74d18750f8
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-30 23:29:01 -0500

    :dart: Improve: agregar prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import "./styles.css";
-import Image from "./react.png";
-import ImageTwo from "./react-1.svg";
+import './styles.css'
+import Image from './react.png'
+import ImageTwo from './react-1.svg'
 export default function App() {
-    return (
-        <div>
-            <h1>
-                React y webpack5 template - {process.env.NODE_ENV} -{" "}
-                {process.env.name}
-            </h1>
-            <img src={Image} alt="React 1" />
-            <img src={ImageTwo} alt="React 2" width="300" />
-        </div>
-    );
+  return (
+    <div>
+      <h1>
+        React y webpack5 template - {process.env.NODE_ENV} - {process.env.name}
+      </h1>
+      <img src={Image} alt="React 1" />
+      <img src={ImageTwo} alt="React 2" width="300" />
+    </div>
+  )
 }
